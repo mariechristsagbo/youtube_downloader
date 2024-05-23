e3d8c96f7258182867dd2bb80884f7df2f6db113
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-05-23 21:29:40 +0100

    update

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from pytube import Playlist, YouTube
 import re
 from pathlib import Path
 
-
 def get_download_path():
     home_directory = Path.home()
     download_dirs = ["Téléchargements", "Downloads", "Telechargements", "Telechargement", "Téléchargement"]
@@ -12,9 +11,7 @@ def get_download_path():
         download_path = home_directory / dir_name
         if download_path.exists() and download_path.is_dir():
             return download_path
-        
     return home_directory / "Downloads"
-
 
 app = Flask(__name__)
 
@@ -29,36 +26,33 @@ def download():
     pattern = r'list=([a-zA-Z0-9_-]+)'
     match = re.search(pattern, url)
 
-    if match:
-        if 'index' not in url:
-            try:
-                playlist = Playlist(url)
-                playlist_title = playlist.title
-                playlist_directory = home_directory / playlist_title
+    try:
+        if match:
+            download_links = []
+            playlist = Playlist(url)
+            playlist_title = playlist.title
+            playlist_directory = home_directory / playlist_title
 
-                for video in playlist.videos:
-                    video.streams.get_highest_resolution().download(output_path=playlist_directory)
-                return 'Téléchargement de la playlist terminé.'
-            except Exception as e:
-                return f'Erreur : {str(e)}'
-        else:
-            try:
-                yt = YouTube(url)
-                video = yt.streams.get_highest_resolution()
-                video.download(output_path = home_directory)
-                return f"Le téléchargement de la vidéo '{video.title}' terminé."
-            except Exception as e:
-                return f'Erreur : {str(e)}'
-    elif 'youtube.com/watch' in url:
-        try:
+            for video in playlist.videos:
+                video_stream = video.streams.get_highest_resolution()
+                video_stream.download(output_path=playlist_directory)
+                download_links.append(playlist_directory / f"{video.title}.mp4")
+            
+            return render_template('index.html', download_links=download_links)
+        
+        elif 'youtube.com/watch' in url:
             yt = YouTube(url)
             video = yt.streams.get_highest_resolution()
-            video.download(output_path = home_directory)
-            return f"Le téléchargement de la vidéo '{video.title}' terminé."
-        except Exception as e:
-            return f'Erreur : {str(e)}'
-    else:
-        return 'URL invalide.'
+            video.download(output_path=home_directory)
+            download_link = home_directory / f"{yt.title}.mp4"
+            
+            return render_template('index.html', download_link=download_link)
+        
+        else:
+            return 'URL invalide.'
+    
+    except Exception as e:
+        return f'Erreur : {str(e)}'
 
 if __name__ == '__main__':
     app.run(debug=True)
